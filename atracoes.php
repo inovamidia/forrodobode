1e4c2927075c105e3b0c766f399cfdb5524ebb10
Everton Paixão <evertonpaixao@gmail.com>
2016-04-14 14:34:37 -0300

    Forró do Bode

--- atracoes.php
+++ atracoes.php
@@ -20,67 +20,19 @@
 
         <!--Section-->
         <section class="section-banner">
-            <img src="img/bg-banner-interna.jpg" alt=""/>
+            <a href="index.php"><img src="img/bg-banner-interna.png" alt=""/></a>
         </section>
         <!--Fim-Section-->
-
+            
         <!--Main-->    	
         <main role="main"> 
         
        		<!--Content-->   
             <div class="content bg">
-                    <div class="box-a">
-                        <h1 class="titulo tit-atracoes">Atrações</h1>
-                         <section class="section col-esq" ><img src="img/durval.png" alt=""/></section>
-                         <article class="section col-dir" >
-                            <h1>Durval Lelys</h1>
-                            <p>
-                                Com um axé de raízes no Rock’n Roll, Durval Lelys é um artista de muita criatividade e costuma, junto com o seu trabalho musical, desenvolver também um lado teatral, através de personagens devidamente caracterizados. Grande artista do carnaval de Salvador, Durval já apresentou na festa momesca personagens como Nero (rodando a manivela), Conde Draculino (o Vampiro Comedor), Salvador Dalino (com o seu pincel) e o Reiciclável. Seu show promete diversos sucessos da sua carreira e músicas que passeiam por diversos estilos musicais.
-                            </p>
-                         </article>
-                    </div>	
-                    
-                    
-                    <div class="box-b">
-                        <section class="section col-esq" ><img src="img/safadao.png" alt=""/></section>
-                        <article class="section col-dir" >
-                            <h1>Wesley Safadão</h1>
-                            <p>
-                                O cantor Wesley Safadão não está no topo das paradas da música do Brasil por acaso. Sempre inovador, o artista é conhecido pelo seu repertório atualizado e por ter as melhores músicas cantadas em seu show. O reconhecimento nacional veio com as primeiras apresentações da banda pelos estados de São Paulo, Minas Gerais e Rio de Janeiro – o Sudeste atualmente é uma das regiões de forte atuação de Wesley Safadão e Garota Safada.
-                            </p>
-                        </article>
-                    </div>
-       
-                    <div class="box-c">
-                        <section class="section col-esq" ><img src="img/timbalada.png" alt=""/></section>
-                        <article class="section col-dir" >
-                            <h1>Timbalada</h1>
-                            <p>
-
-                            </p>
-                        </article>
-                    </div>
-
-                    <div class="box-d">
-                        <section class="section col-esq" ><img src="img/lucco.png" alt=""/></section>
-                        <article class="section col-dir" >
-                            <h1>Lucas Lucco</h1>
-                            <p>
-                            
-                            </p>
-                        </article>
-                    </div> 
-
-                    <div class="box-d">
-                        <section class="section col-esq" ><img src="img/daniel.png" alt=""/></section>
-                        <article class="section col-dir" >
-                            <h1>Danniel Vieira</h1>
-                            <p>
-                                Essência baiana com ritmo sertanejo, essa é a levada do cantor Danniel Vieira que tem esquentado, ainda mais, este gênero musical que hoje conquistou o Brasil. Mesclando um estilo alegre a boas doses de romantismo, o artista é hoje um dos grandes diferenciais da música baiana atual. Ao longo de seus sete anos de carreira, já dividiu palco com artistas e bandas como Luís Caldas, Jorge & Matheus, Victor & Léo, Aviões do Forró e Banda Eva. E mais, se revelou como compositor de canções que caíram no gosto popular, como o hit “I Love You, Baby”, gravado pelo cantor Tomate em seu DVD.
-                            </p>
-                        </article>
-                    </div>  
-
+                <article class="box-esquerdo">
+                    <h1 class="titulo tit-atracoes">Atrações</h1>
+                    <img src="img/img-atracoes.png" alt=""/>
+                </article>
             </div>
             <!--Fim-Content--> 
                 
